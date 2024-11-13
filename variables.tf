013fd9bdee7c7da0c76e61e240fe96b6b71d8db8
jby9725 <jby9725@gmail.com>
2024-11-13 13:34:18 +0900

    테라폼으로 EC2 생성

--- variables.tf
+++ variables.tf
@@ -1,2 +1,17 @@
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
+
+variable "prefix" {
+  description = "Prefix for all resources"
+  default     = "dev"
+}
+
+variable "region" {
+  description = "region"
+  default     = "ap-northeast-2"
+}
+
+variable "nickname" {
+  description = "nickname"
+  default     = "jby9725"
+}
